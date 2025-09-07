c9d199c5f6aab61bae657b74b4ae23d353ca0746
yannismy <i.mylonakis@gmail.com>
2025-09-07 18:38:18 +0300

    Continued analysis

---
+++ main.R
@@ -0,0 +1,9 @@
+library(tidyverse)
+library(here)
+
+
+tv_hours_table <- gss_cat %>% 
+  group_by(marital) %>% 
+  summarize(mean_tv_hours = mean(tvhours, na.rm = TRUE))
+
+write.csv(tv_hours_table, here("TV_Hours_by_Marital.csv")) 
