530557025da61391db97f95a59dfecae9fe63bca
yannismy <i.mylonakis@gmail.com>
2025-09-07 22:45:50 +0300

    Filtered to those under 30.R

--- main.R
+++ main.R
@@ -3,7 +3,7 @@ library(here)
 
 
 tv_hours_table <- gss_cat %>% 
-  filter(age >= 30) %>% 
+  filter(age < 30) %>% 
   group_by(marital) %>% 
   summarize(mean_tv_hours = mean(tvhours, na.rm = TRUE))
 
